234966b1dec171e190a58ded55d67a7a6eddd103
SylvieButeux <sylvie_buteux@yahoo.fr>
2022-11-27 12:51:55 +0100

    mise a jours instrumentation key insight

--- config.py
+++ config.py
@@ -10,13 +10,12 @@ class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 3978
+    # Configuration chat bot
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    # Configuration Luis
     LUIS_APP_ID = os.environ.get("LuisAppId", "5a1ffd5c-f47a-49c7-8f17-2c91c3b53eab")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "ad25e2608a7b496ca39d639f74a0dcd8")
-    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
     LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "https://westeurope.api.cognitive.microsoft.com/")
-    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "AppInsightsInstrumentationKey", "f3001851-bd7c-4c79-b3f7-fa0cc1c30e55"
-                                          
-    )
+    # Configuration app insights
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get("AppInsightsInstrumentationKey", "a519bfb1-ca03-4851-a37e-a3dd4e58b4df"  )
